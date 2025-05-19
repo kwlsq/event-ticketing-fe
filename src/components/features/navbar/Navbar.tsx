7e016b781fd7837eed9955beab0a0141fecc2b09
adi wicaksono <muhammadiwicaksono@gmail.com>
2025-05-19 10:03:16 +0700

    feat : setup fe project

---
+++ src/components/features/navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const Navbar = () => {
+  return (
+    <div>
+      <Button>Signup</Button>
+      <Button variant={"outline"}>Login</Button>
+    </div>
+  );
+}
+
+export default Navbar;
