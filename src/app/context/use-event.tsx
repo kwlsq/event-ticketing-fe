b1baef7b656e34ae2a5ee06705491959faf71fb2
adi wicaksono <muhammadiwicaksono@gmail.com>
2025-05-22 15:18:27 +0700

    feat: create sort by button

--- src/app/context/use-event.tsx
+++ src/app/context/use-event.tsx
@@ -14,6 +14,12 @@ interface EventContextType {
   error: unknown,
   selectedEvent: EventDetailsProps | null,
   setSelectedEvent: (selectedEvent: EventDetailsProps | null) => void,
+  totalPages: number,
+  setTotalPages: (totalPages: number) => void,
+  page: number,
+  setPage: (page: number) => void
+  sort: string,
+  setSort: (sort: string) => void
 }
 
 const EventContext = createContext<EventContextType | undefined>(undefined);
@@ -23,25 +29,29 @@ export const EventProvider = ({ children }: { children: ReactNode }) => {
   const [selectedEvent, setSelectedEvent] = useState<EventDetailsProps | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<unknown>(null);
+  const [totalPages, setTotalPages] = useState(0);
+  const [page, setPage] = useState(0);
+  const [sort, setSort] = useState('id');
 
   useEffect(() => {
     const fetchEventData = async () => {
       try {
-        const response = await axios.get(`${API_URL.BASE_URL_LOCAL}${API_URL.endpoints.event}`);
-        const events: EventProps[] = response.data.content;
+        const response = await axios.get(`${API_URL.BASE_URL_LOCAL}${API_URL.endpoints.event}?page=${page}&size=4&sort=${sort}`);
+        const events: EventProps[] = response.data.data.content;
+        setTotalPages(response.data.data.totalPages);
+        console.log(totalPages);
         setEvents(events);
         setLoading(false);
       } catch (error) {
         setError(error);
         setLoading(false);
-        return [];
       }
     }
     fetchEventData();
-  }, [])
+  }, [page, sort])
 
   return (
-    <EventContext.Provider value={{events, loading, error, selectedEvent, setSelectedEvent}}>
+    <EventContext.Provider value={{events, loading, error, selectedEvent, setSelectedEvent, totalPages, setTotalPages, page, setPage, sort, setSort}}>
       {children}
     </EventContext.Provider>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,30 +3,75 @@
 import EventCard from './components/Event Card/index';
 import DynamicPagination from './components/Dynamic Pagination';
 import { useEvents } from './context/use-event';
+import { Button } from '@/components/ui/button';
+import { Select, SelectContent, SelectItem, SelectLabel, SelectGroup, SelectTrigger, SelectValue } from '@/components/ui/select';
+
+const sortLabels: Record<string, string> = {
+  name: "Sort by Name",
+  venue: "Sort by Venue",
+  location: "Sort by Location",
+  date: "Sort by Date",
+  id: "Sort by"
+}
 
 export default function Home() {
 
-  const { events } = useEvents();
+  const { events, totalPages, setSort, sort } = useEvents();
 
-  if (events !== undefined) {
-    return (
-      <div className="mx-[100px]">
-        <div className="flex flex-wrap gap-5">
-          {events.map((event) => (
-            <div key={event.id} className="w-[272px]">
-              <EventCard
-                name={event.name}
-                date={event.date}
-                venue={event.venue}
-                location={event.location}
-                time={900}
-                startingPrice={event.startingPrice}
-              />
-            </div>
-          ))}
-        </div>
-        <DynamicPagination />
-      </div>
-    );
+  const handleSort = (value: string) => {
+    setSort(value);
   }
+
+  return (
+    <div className="mx-[100px] flex flex-col gap-9">
+      <div className='flex gap-3.5 text-neutral-400 w-full justify-end'>
+        <Button
+          variant={"outline"}
+          className=''
+        >
+          Date
+        </Button>
+        <Button
+          variant={"outline"}
+          className='text-neutral-400'
+        >
+          Price
+        </Button>
+        <Select onValueChange={handleSort} value={sort}>
+          <SelectTrigger className='rounded-full'>
+            <SelectValue>
+              {sortLabels[sort]}
+            </SelectValue>
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>
+                Sort by
+              </SelectLabel>
+              <SelectItem value="name">Name</SelectItem>
+              <SelectItem value="venue">Venue</SelectItem>
+              <SelectItem value="location">Location</SelectItem>
+              <SelectItem value="date">Date</SelectItem>
+              <SelectItem value="id">Default</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+        {events?.map((event) => (
+          <div key={event.id} className="w-[272px]">
+            <EventCard
+              name={event.name}
+              date={event.date}
+              venue={event.venue}
+              location={event.location}
+              time={900}
+              startingPrice={event.startingPrice}
+            />
+          </div>
+        ))}
+      </div>
+      <DynamicPagination totalPages={totalPages} />
+    </div>
+  );
 }
